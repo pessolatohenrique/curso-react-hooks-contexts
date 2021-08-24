1fbb860e246e077c912d941177b81c84b354b013
pessolatohenrique@gmail.com <pessolatohenrique@gmail.com>
2021-08-24 18:44:27 -0300

    Componente comum para loading

---
+++ src/utils/CustomLoader.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Backdrop, CircularProgress, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff",
+  },
+}));
+
+export default function CustomLoader({ isLoading }) {
+  const classes = useStyles();
+
+  return (
+    <Backdrop className={classes.backdrop} open={isLoading}>
+      <CircularProgress color="inherit" />
+    </Backdrop>
+  );
+}
