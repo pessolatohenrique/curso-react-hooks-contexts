05270615beb4cb43b8338513371253a9a6b89a27
pessolatohenrique@gmail.com <pessolatohenrique@gmail.com>
2021-08-26 20:56:27 -0300

    Melhorias no layout - centralizacao do form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { Container, Typography } from "@material-ui/core";
+import { Container, Typography, Card, CardContent } from "@material-ui/core";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+
 import RegisterContainer from "./components/register/RegisterContainer";
 import "@fontsource/roboto";
 import { makeStyles } from "@material-ui/core/styles";
-// import { validateCpf, validateFieldRequired } from "./utils/validation";
-// usar objeto padrão com { valid e message }
-
 export const ThemeContext = React.createContext("dark");
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    marginTop: "20px",
+    display: "flex",
+    flexDirection: "column",
+    height: 600,
+    justifyContent: "center",
   },
 }));
 
@@ -18,48 +20,26 @@ function register(fields) {
   console.log("Fields on register", fields);
 }
 
-function validateCpf(cpf, errors) {
-  let objectError = { ...errors };
-  objectError["cpf"] = { valid: true, message: "" };
-
-  if (cpf.length !== 11) {
-    objectError["cpf"] = { valid: false, message: "O CPF deve ter 11 dígitos" };
-  }
-
-  return objectError;
-}
-
-function validateFieldRequired(field, value, errors) {
-  let objectError = { ...errors };
-  objectError[field] = { valid: true, message: "" };
-
-  if (!value || value.length === 0) {
-    objectError[field] = {
-      valid: false,
-      message: "Campo de preenchimento obrigatório",
-    };
-  }
-
-  console.log("object error", objectError);
-
-  return objectError;
-}
-
 function App() {
   const classes = useStyles();
 
   return (
     <Container maxWidth="md" className={classes.container}>
-      <Typography component="h1" variant="h3" align="center" color="primary">
-        Cadastre-se
-      </Typography>
-      <ThemeContext.Provider value={"dark"}>
-        <RegisterContainer
-          onRegister={register}
-          onValidateCpf={validateCpf}
-          onValidateRequired={validateFieldRequired}
-        />
-      </ThemeContext.Provider>
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography
+            component="h1"
+            variant="h3"
+            align="center"
+            color="primary"
+          >
+            Cadastre-se
+          </Typography>
+          <ThemeContext.Provider value={"dark"}>
+            <RegisterContainer onRegister={register} />
+          </ThemeContext.Provider>
+        </CardContent>
+      </Card>
     </Container>
   );
 }
